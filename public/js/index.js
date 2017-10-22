f3618f15932bdc6a5e35f6a8a0d2fdcd1633299a
Kamlesh Kumar <kamlesh.kumar@jadeglobal.com>
2017-10-22 13:22:13 +0530

    emit new message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,7 @@
 
     socket.on('connect',function() {
       console.log('Connected to server');
-
-    socket.emit('createMessage', {
-    to: 'kamal@example.com',
-    text: 'Hey. This is kamal.'
-  });
+ 
     });
 
     socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage',{
-    from:'kamlesh.ask90@gmail.com',
-    text:'Welcome Email',
-    createdAt:12123
-  });
 
   socket.on('createMessage', (message) => {
   console.log('createMessage', message);
+  io.emit('newMessage',{
+    from:message.from,
+    text:message.text,
+    createdAt:new Date().getTime()
+  });
   });
 
   socket.on('disconnect', () => {
